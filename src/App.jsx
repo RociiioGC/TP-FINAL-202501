28cd5e657066ea20339a9bda3ecf26ee677a0f8b
Rocio Garcia Cabrera <rociiio.gc@hotmail.com>
2025-12-01 21:22:11 +0000

    Organizando la base del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Container, VStack, Text, Tag } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box minH="100vh" bg="#c9b4f9" py={10}>
+      <Container maxW="sm">
+        <Box
+          bg="#fdfefe"
+          borderRadius="3xl"
+          p={8}
+          boxShadow="xl"
+          bgImage="radial-gradient(circle, #d4d4d4 1px, transparent 1px)"
+          bgSize="16px 16px"
+        >
+          <VStack spacing={3} mb={4}>
+            <Tag
+              bg="#3af5c3"
+              color="black"
+              px={6}
+              py={3}
+              borderRadius="full"
+              fontWeight="700"
+              fontSize="lg"
+              boxShadow="md"
+            >
+              To-Do List
+            </Tag>
+
+            <Text fontSize="sm" color="gray.700">
+              ~ Hoy tengo que hacer ~
+            </Text>
+
+            {/* CONTADOR DE TAREAS */}
+            <Text fontSize="sm" color="gray.600" mt={2}>
+              You have <b>3</b> task(s) to complete
+            </Text>
+          </VStack>
+
+          {/* ACÁ AGREGAR EL FORM Y LA LISTA */}
+          <Box mt={6}>
+            <Text fontSize="sm" color="gray.500" textAlign="center">
+              (Aquí va la lista de tareas con iconos)
+            </Text>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
 }
 
-export default App
+export default App;
