586731b80fabd2372bb39b57d1ceeb7ed06c5207
Rocio Garcia Cabrera <rociiio.gc@hotmail.com>
2025-12-04 10:50:36 +0000

    creacion del componente Modal para alertas

---
+++ src/components/Modal.jsx
@@ -0,0 +1,14 @@
+const Modal = ({ isOpen, onClose, children }) => {
+    if (!isOpen) return null;
+  
+    return (
+      <div className="overlay" onClick={onClose}>
+        <div className="modal" onClick={(e) => e.stopPropagation()}>
+          {children}
+          <button className="close-btn" onClick={onClose}>Cerrar</button>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Modal;
